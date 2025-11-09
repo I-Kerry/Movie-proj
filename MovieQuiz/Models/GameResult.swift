f334e234d0ecca0f243905f45daed77b021afed6
Trashcor <trashmencor@inbox.ru>
2025-11-09 17:41:38 +0800

    fix: restracure code and implement statistics settings

---
+++ MovieQuiz/Models/GameResult.swift
@@ -0,0 +1,16 @@
+
+import Foundation
+
+struct GameResult {
+    let correct: Int
+    let total: Int
+    let date: Date
+    
+    func isBetterThan(_ another:GameResult) -> Bool {
+        if correct > another.correct {
+            return true
+        } else {
+            return false
+        }
+    }
+}
---
+++ MovieQuiz/Models/QuizResultsViewModel.swift
@@ -0,0 +1,10 @@
+
+
+import Foundation
+
+struct QuizResultViewModel {
+    let title: String
+    let text: String
+    let buttonText: String
+}
+
